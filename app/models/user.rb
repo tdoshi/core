edd6c7efdfb81811152fb481fd3688850f4d8fdd
Rahul Pandey <rpandey1234@gmail.com>
2014-01-02 22:41:48 -0500

    fix facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      # TODO: ask user for email
+      user.email = "#{auth.uid}_email@example.com"
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
