f3866ea27dabae3644f1b84573bef135cb5b0285
Rahul Pandey <rpandey1234@gmail.com>
2014-04-18 00:01:26 -0700

    copied project from cs147, it runs

---
+++ models.js
@@ -0,0 +1,13 @@
+
+var Mongoose = require('mongoose');
+
+var ProjectSchema = new Mongoose.Schema({
+	"title": String,
+	"date": Date,
+	"summary": String,
+	"image": String
+});
+
+exports.Project = Mongoose.model('Project', ProjectSchema);
+
+
