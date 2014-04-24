9d543c6b6c8660e5866c7cee25cd250bda31f2a8
Rahul Pandey <rpandey1234@gmail.com>
2014-04-24 01:54:46 -0700

    youtube player loading correctly

--- public/js/controllers/AnnCtrl.js
+++ public/js/controllers/AnnCtrl.js
@@ -1,16 +1,12 @@
-var AnnCtrl = function($scope, $http) {
+var AnnCtrl = function($scope, $http, $window) {
   // The whole annotation, which includes many individual annotations
   $scope.whole = {
   	video_link: "https://www.youtube.com/watch?v=7IoRIj9XQfQ",
   	video_id: ""
   };
+  $scope.player = {};
   $scope.error = "";
-
-  // Load the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/player_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  $scope.youtubeLoaded = false;
 
   $scope.loadVideo = function(youtubeLink) {
   	var re = /v=([\w-]+)/;
@@ -18,26 +14,33 @@ var AnnCtrl = function($scope, $http) {
   	if (matched.length != 2) {
   		$scope.error = 'failed to parse the youtube link';
   		console.log($scope.error);
+  		// TODO: better error checking here
+  		return;
+  	}
+  	// All Youtube video ids are 11 characters
+  	if (matched[1].length != 11) {
+  		// TODO: better error checking here
+  		$scope.error = 'that is not a valid Youtube ID'
   		return;
   	}
   	$scope.whole.video_id = matched[1];
   	console.log($scope.whole.video_id);
-		// Replace the 'ytplayer' element with an <iframe> and
-	  // YouTube player after the API code downloads.
-	  var player;
-	  console.log("player", player);
-	  function onYouTubePlayerAPIReady() {
-	  	console.log('loaded yt player API');
-	    player = new YT.Player('ytplayer', {
+  	if ($scope.youtubeLoaded) {
+  		$scope.player = new YT.Player('ytplayer', {
 	      height: '390',
 	      width: '640',
-	      videoId: 'M7lc1UVf-VE'
+	      videoId: $scope.whole.video_id
 	    });
-	  }
-
+  	}
   };
+
+  // This gets called once the Youtube iframe API code has loaded
+  $window.onYouTubeIframeAPIReady = function() {
+  	$scope.youtubeLoaded = true;
+  	console.log('loaded yt player API');
+  }
 
 };
 
-AnnCtrl.$inject = ['$scope', '$http'];
+AnnCtrl.$inject = ['$scope', '$http', '$window'];
 
