30461a697a49bdf8a69e9025b65f4b168b369985
Rahul Pandey <rpandey1234@gmail.com>
2014-04-24 03:17:09 -0700

    able to seek to time in video, record notes

--- public/js/controllers/AnnCtrl.js
+++ public/js/controllers/AnnCtrl.js
@@ -2,11 +2,24 @@ var AnnCtrl = function($scope, $http, $window) {
   // The whole annotation, which includes many individual annotations
   $scope.whole = {
   	video_link: "https://www.youtube.com/watch?v=7IoRIj9XQfQ",
-  	video_id: ""
+  	video_id: "7IoRIj9XQfQ"
   };
   $scope.player = {};
   $scope.error = "";
   $scope.youtubeLoaded = false;
+  $scope.cur_ann = {};
+
+  $scope.annotations = [];
+  $scope.allowAnnotation = false;
+  // TODO: make this frozen so it cannot be changed
+  VideoStatusEnum = Object.freeze({
+  	UNSTARTED: -1,
+  	ENDED: 0,
+  	PLAYING: 1,
+  	PAUSED: 2,
+  	BUFFERING: 3,
+  	VIDEO_CUED: 5
+  });
 
   $scope.loadVideo = function(youtubeLink) {
   	var re = /v=([\w-]+)/;
@@ -34,9 +47,35 @@ var AnnCtrl = function($scope, $http, $window) {
   	}
   };
 
+  $scope.annotate = function() {
+  	var state = $scope.player.getPlayerState();
+  	// TODO: robust casework, what about buffering?
+  	if (state == VideoStatusEnum.PLAYING) {
+  		$scope.allowAnnotation = true;
+  		$scope.player.pauseVideo();
+  		$scope.cur_ann.time = Math.floor($scope.player.getCurrentTime());
+  	} else if (state == VideoStatusEnum.PAUSED || state == VideoStatusEnum.UNSTARTED || state == VideoStatusEnum.VIDEO_CUED || state == VideoStatusEnum.ENDED) {
+  		$scope.player.playVideo();
+  	}
+  };
+
+  $scope.completeAnnotation = function() {
+  	$scope.allowAnnotation = false;
+  	$scope.annotations.push($scope.cur_ann);
+  	$scope.cur_ann = {};
+		console.log('hi', $scope.annotations);
+  	$scope.player.playVideo();
+  };
+
+  $scope.examineAnn = function(ann) {
+  	console.log('time of ann:', ann.time);
+  	$scope.player.seekTo(ann.time);
+  };
+
   // This gets called once the Youtube iframe API code has loaded
   $window.onYouTubeIframeAPIReady = function() {
   	$scope.youtubeLoaded = true;
+  	$scope.loadVideo();
   	console.log('loaded yt player API');
   }
 
